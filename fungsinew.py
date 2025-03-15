fa9a18f56995ad8b6ae8c01d1a6d0eeb53f52881
Shelford21 <122199835+Shelford21@users.noreply.github.com>
2025-03-15 22:21:46 +0700

    Update fungsinew.py

--- fungsinew.py
+++ fungsinew.py
@@ -1086,7 +1086,9 @@ if st.session_state["current_page"] == "🩻 Evaluation":
             ax.set_facecolor('black')
 
                 # Seaborn barplot
-            sns.barplot(x=sentiment_counts.index, y=sentiment_counts.values, palette='Blues', ax=ax)
+            sns.barplot(x=sentiment_counts.index, y=sentiment_counts.values, 
+            palette=sns.light_palette("lime", as_cmap=True), ax=ax)
+
 
 
                 # Apply a glow effect on the borders
