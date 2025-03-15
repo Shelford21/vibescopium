ca834e12fe3919c70f59d3472b66907a859b3f10
Shelford21 <122199835+Shelford21@users.noreply.github.com>
2025-03-15 22:28:47 +0700

    Update fungsinew.py

--- fungsinew.py
+++ fungsinew.py
@@ -1087,7 +1087,7 @@ if st.session_state["current_page"] == "🩻 Evaluation":
 
                 # Seaborn barplot
             sns.barplot(x=sentiment_counts.index, y=sentiment_counts.values, 
-            palette=sns.color_palette(["#05ffea", "#ff05ee"]), ax=ax)
+            palette=sns.color_palette(["#00FFFF", "#ff00ff"]), ax=ax)
 
 
 
