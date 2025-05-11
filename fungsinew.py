00acc9283eea9d895fca442dd6b5e96340dddd56
Shelford21 <122199835+Shelford21@users.noreply.github.com>
2025-05-11 07:57:23 +0700

    Update fungsinew.py

--- fungsinew.py
+++ fungsinew.py
@@ -654,7 +654,7 @@ if st.session_state["current_page"] == "DataFrames":
             
              
         else:
-             st.dataframe(clean_df[['content', 'score','thumbsUpCount','at','appVersion','text_clean', 'text_casefolding','text_slang_fixed','text_tokenized','text_stopword',
+            st.dataframe(clean_df[['content', 'score','thumbsUpCount','at','appVersion','text_clean', 'text_casefolding','text_slang_fixed','text_tokenized','text_stopword',
                                             #'text_stemming',
                                             'text_akhir', 'polarity_score', 'polarity']],use_container_width=True , height=6000) 
            # Let user select the column to filter by
