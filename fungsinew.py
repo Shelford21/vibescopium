99cf0d2e3de72ec2fe1d401f2e48e8f3e4d7a9ff
Shelford21 <122199835+Shelford21@users.noreply.github.com>
2025-05-18 13:09:10 +0700

    Update fungsinew.py

--- fungsinew.py
+++ fungsinew.py
@@ -462,7 +462,7 @@ if st.session_state["current_page"] == "Input App ID":
                         csv_writer = csv.writer(output)
                         csv_writer.writerow(['Review'])
                         for review in reviews:
-                            csv_writer.writerow([review['content', 'score','thumbsUpCount','at','appVersion']])
+                            csv_writer.writerow([review['content']])
                         return output.getvalue().encode('utf-8')
                 
                     csv_bytes = generate_csv(reviews)
