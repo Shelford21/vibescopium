be75e899d74551015e421a3beb75a0ecaea0cdc4
Shelford21 <122199835+Shelford21@users.noreply.github.com>
2025-05-20 11:00:33 +0700

    Update fungsinew.py

--- fungsinew.py
+++ fungsinew.py
@@ -690,7 +690,7 @@ if st.session_state["current_page"] == "Input App ID":
             #     st.warning("CSV belum dibuat. Silakan generate CSV terlebih dahulu.")
 
         except Exception as e:
-            st.write(e)
+            st.write("_")
         
                     
 
